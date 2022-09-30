fca4f2d72730047cb61cb401635c9961912917f8
Joseph C <43707388+Trip1eLift@users.noreply.github.com>
2022-09-29 22:01:39 -0400

    Setup go websocket server with healthcheck endpoint

---
+++ go-server/README.md
@@ -0,0 +1,36 @@
+launch go from local
+```
+go mod download
+go run ./main.go
+```
+
+launch go from docker
+```
+docker build --tag virtual-avatar-stream .; docker run -it -p 5001:5001 virtual-avatar-stream
+```
+
+wscat
+```
+npm install -g wscat
+wscat -c ws://127.0.0.1:5001/
+```
+
+curl ws
+```
+curl -i -N http://127.0.0.1:5001/ \
+-H "Connection: Upgrade" \
+-H "Upgrade: websocket" \
+-H "Sec-Websocket-Version: 13" \
+-H "Sec-Websocket-Key: mock" \
+-H "Health: healthcheck" || exit 1
+```
+
+curl ws on cmd
+```
+curl -i -N http://127.0.0.1:5001/ ^
+-H "Connection: Upgrade" ^
+-H "Upgrade: websocket" ^
+-H "Sec-Websocket-Version: 13" ^
+-H "Sec-Websocket-Key: mock" ^
+-H "Health: healthcheck" || exit 1
+```
---
+++ go-server/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const port = "5001"
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// TODO: handle client close here?
+
+		messageType, body, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(body))
+
+		if err := conn.WriteMessage(messageType, body); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(write http.ResponseWriter, request *http.Request) {
+	upgrader.CheckOrigin = wsHeaderCheck
+	response_header := http.Header{
+		"Status": {"Connection-Established"},
+	}
+	ws, err := upgrader.Upgrade(write, request, response_header)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("Client Successfully Connected...")
+
+	if close := wsHealthCheck(ws, request); close == true {
+		return
+	}
+	reader(ws)
+}
+
+func wsHeaderCheck(request *http.Request) bool {
+	if request.Header["Connection"] == nil || request.Header["Connection"][0] != "Upgrade" {
+		log.Println("Header Connection is not [Upgrade] but", request.Header["Connection"])
+		return false
+	}
+	if request.Header["Upgrade"] == nil || request.Header["Upgrade"][0] != "websocket" {
+		log.Println("Header Upgrade is not [websocket] but", request.Header["Upgrade"])
+		return false
+	}
+	if request.Header["Sec-Websocket-Key"] == nil {
+		log.Println("Header Sec-Websocket-Key is missing")
+		return false
+	}
+	return true
+}
+
+func wsHealthCheck(conn *websocket.Conn, request *http.Request) bool {
+	if request.Header["Health"] == nil || request.Header["Health"][0] != "healthcheck" {
+		return false
+	}
+	log.Println("This is a health check connection. Instance is healthy. Terminating...")
+	// 8: CloseMessage, 1000: CloseNormalClosure
+	if err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "Instance is healthy!")); err != nil {
+		log.Println(err)
+	}
+	if err := conn.Close(); err != nil {
+		log.Println(err)
+	}
+	return true
+}
+
+func main() {
+	http.HandleFunc("/", wsEndpoint)
+	http.HandleFunc("/health", func(write http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(write, "Healthy")
+	})
+	log.Println("Listening on localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
